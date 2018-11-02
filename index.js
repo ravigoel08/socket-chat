1b21ddeb1c86b40ce9ba01df3b7451ef6e99aed0
ravigoel08 <ravigoel.1997@gmail.com>
2018-11-03 00:51:53 +0530

    repeat again

--- index.js
+++ index.js
@@ -2,22 +2,24 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+//app.get('/', function(req, res){       routing url to /
+    //res.send("<h1>Hello World!</h1>");        rendering html 
+//});
 app.get('/', function(req, res){
-    //res.send('<h1>Hello world</h1>');   for display this tag
     res.sendFile(__dirname + '/index.html');
 });
-
+var msgg = "welcome to chat";
 io.on('connection', function(socket){
-    
-    //console.log('a user connected'); for show on console if a usr connect or disconnect
+    //console.log('a user connected');   //to show on console if a user connects or disconnect
     //socket.on('disconnect', function(){
         //console.log('user disconnected');
+    //});
     socket.on('chat message', function(msg){
-        //console.log('message:' +msg);  showing message on console
-        io.emit('chat message', msg)
+        //console.log('message:' + msg);    to show message on console
+        io.emit('chat message', msg);
     });
 });
 
 http.listen(3000, function(){
-    console.log('listening  on *:3000');
+    console.log('listening on *:3000');
 });
